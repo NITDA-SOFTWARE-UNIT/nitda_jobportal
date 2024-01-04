c3aee919148f12d10290a54a0d460138ab576958
magama-star <mmaagama@nitda.gov.ng>
2024-01-04 13:25:15 +0100

    first commit on auth setup

---
+++ app.py
@@ -0,0 +1,93 @@
+import os
+import time
+from flask import Flask, abort, request, jsonify, g, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_httpauth import HTTPBasicAuth
+import jwt
+from werkzeug.security import generate_password_hash, check_password_hash
+
+#Initialize variables
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'use a random string to construct the hash'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+# Extensions
+db = SQLAlchemy(app)
+auth = HTTPBasicAuth()
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(32), index = True)
+    password_hash = db.Column(db.String(64))
+
+
+    def hash_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+    def generate_auth_token(self, expires_in = 600):
+        return jwt.encode(
+            { 'id': self.id, 'exp': time.time() + expires_in }, 
+            app.config['SECRET_KEY'], algorithm='HS256')
+
+    @staticmethod
+    def verify_auth_token(token):
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'],
+            algorithm=['HS256'])
+        except:
+            return 
+        return User.query.get(data['id'])
+
+@auth.verify_password
+def verify_password(username_or_token, password):
+    # first try token
+    user = User.verify_auth_token(username_or_token)
+    # then check for username and password pair
+    if not user:
+        user = User.query.filter_by(username = username_or_token).first()
+        if not user or not user.verify_password(password):
+            return False
+    g.user = user
+    return True
+
+@app.route('/api/register', methods=['POST'])
+def register():
+    username = request.json.get('username') 
+    password = request.json.get('password')
+    # Check for blank requests
+    if username is None or password is None:
+        abort(400)
+    # Check for existing users
+    if User.query.filter_by(username = username).first() is not None:
+        abort(400)
+    user = User(username = username)
+    user.hash_password(password)
+    db.session.add(user)
+    db.session.commit()
+    return (jsonify({'username': user.username}), 201)
+
+
+@app.route('/api/login')
+@auth.login_required
+def get_token():
+    token = g.user.generate_auth_token(600)
+    return jsonify({ 'token': token.decode('ascii'), 'duration': 600 })
+
+
+@app.route('/api/dothis', methods=['GET'])
+@auth.login_required
+def do_this():
+    return jsonify({ 'message':'It is done {}'.format(g.user.username) })
+
+
+if __name__ == "__main__":
+    if not os.path.exists('db.sqlite'):
+        db.create_all()
+    app.run(debug=True)
